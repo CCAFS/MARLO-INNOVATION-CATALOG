7a7af00d5f359746a7b9bae2b02041283d63cd4d
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-10-01 10:37:05 -0500

    ✨ refactor(MapFilter): simplify SVG rendering by creating SvgCircle component for better maintainability and code clarity

---
+++ client/src/pages/index/vue/SvgCircle.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const props = defineProps<{
+  class: string;
+  filter: string;
+  x: number;
+  y: number;
+  fill: string;
+  activeIndex: number;
+  index: number;
+  pathD: string;
+}>();
+
+const emit = defineEmits<{
+  click: [index: number];
+}>();
+</script>
+<template>
+  <g :class="class" :filter="filter" @click="emit('click', index)">
+    <rect :x="x" :y="y" width="37" height="37" rx="18.5" :fill="fill" shape-rendering="crispEdges"></rect>
+    <rect
+      :x="x + 2.5"
+      :y="y + 2.5"
+      width="32"
+      height="32"
+      rx="16"
+      :stroke="activeIndex === index ? '#F39619' : 'white'"
+      stroke-width="5"
+      shape-rendering="crispEdges"></rect>
+    <path :d="pathD" fill="white"></path>
+  </g>
+</template>
