60c7d679f8464fe790bdfdc6b5a57895068a34c0
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-10-02 08:01:32 -0500

    ✨ feat(ReadinessExplorer): add 'see innovations' text to enhance user guidance and improve layout structure

--- client/src/content/texts.ts
+++ client/src/content/texts.ts
@@ -3,6 +3,7 @@ export const texts = {
     overviewStatsTitle: 'Explore Validated Climate Innovations for Africa',
     ReadinessExplorerTitle: 'Innovations by Scaling Readiness',
     ReadinessExplorerDescription:
-      '<strong class="font-bold"> Scaling Readiness is a framework that helps individuals and organizations effectively scale innovations to achieve meaningful impact.</strong> By combining evidence-based strategies, standardized tools, and capacity development, Scaling Readiness empowers teams to identify bottlenecks, manage innovation portfolios, and monitor progress. It is essential for those aiming to bring innovations to scale in a systematic, strategic, and impactful way.'
+      '<strong class="font-bold"> Scaling Readiness is a framework that helps individuals and organizations effectively scale innovations to achieve meaningful impact.</strong> By combining evidence-based strategies, standardized tools, and capacity development, Scaling Readiness empowers teams to identify bottlenecks, manage innovation portfolios, and monitor progress. It is essential for those aiming to bring innovations to scale in a systematic, strategic, and impactful way.',
+    seeInnovations: 'See the relevant innovations below'
   }
 };
--- client/src/pages/index/vue/ReadinessExplorer.vue
+++ client/src/pages/index/vue/ReadinessExplorer.vue
@@ -2,13 +2,20 @@
 import MapFilter from './MapFilter.vue';
 import InnovationFilters from './InnovationFilters.vue';
 import PrimeProvider from './../../../providers/PrimeProvider.vue';
+import { texts } from './../../../content/texts.ts';
 </script>
 
 <template>
-  <div class="w-full bg-gradient-to-r from-[#DFF5FF] to-[#B1D4C5] p-10 rounded-lg mt-10 grid grid-cols-[525px_auto] gap-4">
-    <MapFilter />
-    <PrimeProvider>
-      <InnovationFilters />
-    </PrimeProvider>
+  <div class="bg-gradient-to-r from-[#DFF5FF] to-[#B1D4C5] p-10 pb-6">
+    <div class="w-full rounded-lg mt-10 grid grid-cols-[525px_auto] gap-4">
+      <MapFilter />
+      <PrimeProvider>
+        <InnovationFilters />
+      </PrimeProvider>
+    </div>
+
+    <div class="flex flex-1 w-full justify-center text-[#439255] text-lg font-light gap-4 items-center">
+      {{ texts.home.seeInnovations }} <i class="pi pi-arrow-down block"></i>
+    </div>
   </div>
 </template>
