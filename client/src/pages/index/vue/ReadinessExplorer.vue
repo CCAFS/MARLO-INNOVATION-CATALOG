10d45f2ab790d68bb15f9d9d4663d445f6ea2ab8
JhonSGIzquierdo <53907237+MetalPrime@users.noreply.github.com>
2025-10-06 22:31:48 -0500

    🎨 style: update text size in responsive views

--- client/src/pages/index/vue/ReadinessExplorer.vue
+++ client/src/pages/index/vue/ReadinessExplorer.vue
@@ -14,8 +14,8 @@ import { texts } from './../../../content/texts.ts';
       </PrimeProvider>
     </div>
 
-    <div class="flex flex-1 w-full bg-gradient-to-r from-[#DFF5FF] to-[#B1D4C5] justify-center text-[#439255] text-lg font-light gap-4 items-center pb-4 rounded-b-lg">
-      {{ texts.home.seeInnovations }} <i class="pi pi-arrow-down block"></i>
+    <div class="flex flex-1 w-full bg-gradient-to-r from-[#DFF5FF] to-[#B1D4C5] justify-center text-[#439255] text-sm xl:text-base 2xl:text-lg font-light gap-2 items-center pb-4 rounded-b-lg">
+      {{ texts.home.seeInnovations }} <i class="pi pi-arrow-down block w-3 xl:w-4 2xl:w-4.5"></i>
     </div>
   </div>
 </template>
