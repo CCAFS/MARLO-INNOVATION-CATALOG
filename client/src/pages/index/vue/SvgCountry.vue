ea63e9281a4359af91cfa32cc94f1c142918c24a
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-10-27 11:43:42 -0500

    ✨feat(AfricaSvg): refactor AfricaSvg component to utilize dynamic country paths and create a new SvgCountry component for better modularity; add AfricaSVGPaths for SVG path data management

---
+++ client/src/pages/index/vue/SvgCountry.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps<{
+  id: string;
+  title: string;
+  pathD: string;
+  fill: string;
+  stroke: string;
+}>();
+console.log(props.id);
+console.log('ho');
+</script>
+
+<template>
+  <path :d="props.pathD" :title="props.title" :fill="props.fill" :stroke="props.stroke" :stroke-width="1" :id="props.id" />
+</template>
