9506c34c21d893e3941871576fa6eaa666e208d7
JhonSGIzquierdo <53907237+MetalPrime@users.noreply.github.com>
2025-10-31 17:54:19 -0500

    🔧chore(api): update headers and params in api

--- client/src/composables/useApi.ts
+++ client/src/composables/useApi.ts
@@ -1,6 +1,12 @@
 import { useApiRequest } from './useApiRequest';
 import type { User, Post, CreatePostRequest, CreatePostResponse } from '~/interfaces/api-example.interface';
-import type { InnovationCatalogV2, InnovationCatalogV2Stats } from '~/interfaces/innovation-catalog-v2.interface';
+import {
+  type InnovationType,
+  type InnovationCatalogV2,
+  type InnovationCatalogV2Stats,
+  type SdgResume,
+  type PDFInfo
+} from '~/interfaces/innovation-catalog-v2.interface';
 import type { SearchComplete } from '~/interfaces/search-complete.interface';
 
 export function useApi() {
@@ -42,7 +48,19 @@ export function useApi() {
       innovationTypeId?: number;
       sdgId?: number;
       countryId?: number;
-    }) => makeRequest<SearchComplete>('GET', `${apiBaseUrl}/innovations/search-complete`, { params })
+    }) => makeRequest<SearchComplete>('GET', `${apiBaseUrl}/innovations/search-complete`, { params }),
+
+    getInnovationPDFById: (id: string | number) =>
+      makeRequest<PDFInfo>('GET', `${apiBaseUrl}/innovations/pdf/url/custom?innovationId=${id}`, {
+        params: {
+          cycle: 'Reporting',
+          year: 2025
+        }
+      }),
+
+    getInnovationTypes: () => makeRequest<InnovationType[]>('GET', `${apiBaseUrl}/innovations/innovation-types`),
+
+    getSustainableDevelopmentGoals: () => makeRequest<SdgResume[]>('GET', `${apiBaseUrl}/sustainable-development-goals`)
   };
 }
 
