9afc8fc93cac3520aa63acd91d31af2b96d8ac61
JhonSGIzquierdo <53907237+MetalPrime@users.noreply.github.com>
2025-11-12 18:33:59 -0500

    🔧chore: Remove server-side render and cloudflare function

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -4,14 +4,10 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import vue from '@astrojs/vue';
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  output: 'static',
   vite: {
     plugins: [tailwindcss()]
   },
