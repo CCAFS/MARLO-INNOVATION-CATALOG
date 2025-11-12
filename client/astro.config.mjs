8b1ecd98e0f8ff48c8b2bde7f1880e2c7033fc6c
JhonSGIzquierdo <53907237+MetalPrime@users.noreply.github.com>
2025-11-12 17:24:59 -0500

    🔧chore: Add temp adapter for cloudflare request

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -4,11 +4,16 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import vue from '@astrojs/vue';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss()]
   },
 
   integrations: [vue()]
