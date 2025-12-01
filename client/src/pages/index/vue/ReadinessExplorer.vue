54c8eb8b7e394291c61ae3eeab15f2563bfecaf7
JhonSGIzquierdo <53907237+MetalPrime@users.noreply.github.com>
2025-12-01 15:26:02 -0500

    🔧chore(MapFilter): add interaction in info note and responsive

--- client/src/pages/index/vue/ReadinessExplorer.vue
+++ client/src/pages/index/vue/ReadinessExplorer.vue
@@ -10,7 +10,7 @@ import { texts } from './../../../content/texts.ts';
   <div class="container mx-auto mt-6 px-4 lg:mt-10 lg:p-0 xl:px-16 2xl:px-20 h-full overflow-hidden">
     <div class="w-full bg-gradient-to-r from-[#DFF5FF]/[.50] to-[#B1D4C5]/[.74] rounded-lg">
       <!-- Mobile: 1 columna vertical | Desktop (md+): grid 2 columnas | Desktop (lg+): proporciones originales -->
-      <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-[2fr_1.75fr] xl:grid-cols-[2fr_2.25fr] 2xl:grid-cols-[2fr_2.75fr]">
+      <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-[2fr_1.75fr] xl:grid-cols-[2fr_2.25fr] 2xl:grid-cols-[2fr_2.75fr] relative">
         <MapFilter />
         <PrimeProvider>
           <InnovationFilters />
